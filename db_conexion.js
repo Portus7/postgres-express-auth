e17630fd2e2b7fb26ddf1639aa2cd9077c8200ba
Portus7 <luisporta7@hotmail.com>
2025-11-18 07:17:35 -0300

    Implementacion query auth_db

--- db_conexion.js
+++ db_conexion.js
@@ -35,9 +35,7 @@ app.get('/health', async (req, res) => {
 // 🔁 EJEMPLO: supongamos que tienes una tabla "customers"
 app.get('/auth_db', async (req, res) => {
   try {
-    const result = await pool.query(`SELECT tablename
-FROM pg_tables
-WHERE schemaname NOT IN ('pg_catalog', 'information_schema');`);
+    const result = await pool.query('SELECT * FROM auth_db');
     res.json(result.rows);
   } catch (err) {
     console.error(err);
